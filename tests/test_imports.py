4d56f68e3c082568f3d78a1a543452321a58f0f1
Muhammad Maikudi ISAH <muhammadmaikudi.isah@gmail.com>
2025-12-29 14:27:16 +0100

    update tests imports

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,9 +1,16 @@
+# import os, sys
+# ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "./"))
+# sys.path.insert(0, ROOT)
+# # print(sys.path)
+
+import pytest
+
 def test_import_lmfit_global():
     from lmfit_global import LmfitGlobal
 
 def test_import_utils():
-    from lmfit_global.util import parameters, plotting, reporting, utils
+    from lmfit_global.util import lineshapes, parameters, plotting, reporting, utils, io
 
 def test_utils_helper():
-    from lmfit_global.util.utils import parse_xrange
+    from lmfit_global.util.utils import parse_xrange, GlobalFitBuilder
     assert callable(parse_xrange)
